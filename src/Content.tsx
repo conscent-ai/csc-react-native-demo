a81edc527539eb6a768e4a921f3941f140d376c6
Roshan Kumar Sharma <roshan@conscent.ai>
2024-09-09 15:26:53 +0530

    user agent

--- src/Content.tsx
+++ src/Content.tsx
@@ -18,12 +18,24 @@ import {
 } from 'csc-react-native-sdk';
 import { EventRegister } from 'react-native-event-listeners';
 import Toast from 'react-native-toast-message';
+import DeviceInfo from 'react-native-device-info';
 
 export default function Content(props: any) {
     const paywallRef = useRef(null);
     const [scrollY, setScrollY] = useState(0);
     const [showContent, setShowContent] = useState(false);
     const { contentId, clientId, mode } = props?.route?.params;
+    const [userAgent, setUserAgent] = useState('')
+
+    useEffect(() => {
+        const loadData = async () => {
+            const userAgent1 = await DeviceInfo.getUserAgent();
+            console.log(userAgent1);
+
+            setUserAgent(userAgent1)
+        };
+        loadData();
+    }, [])
 
     const text = [
         'Where does it come from?\n\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
@@ -137,22 +149,22 @@ export default function Content(props: any) {
                 position='top'
                 bottomOffset={20}
             />
-            <PayWall
-                ref={paywallRef}
-                clientId={clientId}
-                contentId={contentId}
-                environment={mode}
-                fontFamily={'PlayfairDisplay-Regular'}
-                userAgent={
-                    'Dalvik/2.1.0 (Linux; U; Android 12; RMX2121 Build/SP1A.210812.016'
-                }
-                currentStackName={'Content'}
-                navigation={props?.navigation}
-                scrollY={scrollY}
-                goBack={() => {
-                    goBack();
-                }}
-            />
+            {userAgent && (
+                <PayWall
+                    ref={paywallRef}
+                    clientId={clientId}
+                    contentId={contentId}
+                    environment={mode}
+                    fontFamily={'PlayfairDisplay-Regular'}
+                    userAgent={userAgent}
+                    currentStackName={'Content'}
+                    navigation={props?.navigation}
+                    scrollY={scrollY}
+                    goBack={() => {
+                        goBack();
+                    }}
+                />
+            )}
             <PopUp
                 environment={mode}
                 currentStackName={'Content'}
