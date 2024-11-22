e7accb1f16d5a55219d27afaa774ffb14ae879f4
Roshan Kumar Sharma <roshan@conscent.ai>
2024-11-22 17:20:30 +0530

    facebook login working in ios

--- ios/csc_demo/AppDelegate.h
+++ ios/csc_demo/AppDelegate.h
@@ -1,5 +1,8 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import <SafariServices/SafariServices.h>
+#import <AuthenticationServices/AuthenticationServices.h>
+
 
 @interface AppDelegate : RCTAppDelegate
 
--- ios/csc_demo/AppDelegate.mm
+++ ios/csc_demo/AppDelegate.mm
@@ -1,6 +1,8 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <FBSDKCoreKit/FBSDKCoreKit-Swift.h>
+
 
 @implementation AppDelegate
 
@@ -10,8 +12,26 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+                            didFinishLaunchingWithOptions:launchOptions];
+
+
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  
+  if ([[FBSDKApplicationDelegate sharedInstance] application:app openURL:url options:options]) {
+    return YES;
+  }
+  
+  return NO;
+  
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
